082623a7c1102f51a369895beb23768295c1e9ce
xuwei <starkcoder@sina.com>
2020-09-29 21:55:38 +0800

    init/nodemon

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 /*
  * @Author: xuwei
  * @Date: 2020-08-10 12:33:53
- * @LastEditTime: 2020-08-10 12:35:26
+ * @LastEditTime: 2020-09-29 12:43:22
  * @LastEditors: xuwei
  * @Description:
  */
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Notification } = require("electron");
+
+app.setAppUserModelId(process.execPath);
 
 function createWindow() {
   // 创建浏览器窗口
@@ -16,12 +18,17 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
-
   // 并且为你的应用加载index.html
-  win.loadFile("index.html");
+  win.loadFile("./renderer/index.html");
+
+  console.info("support", Notification.isSupported());
+  const myNotification = new Notification("Title", {
+    body: "Lorem Ipsum Dolor Sit Amet",
+  });
+  myNotification.show();
 
   // 打开开发者工具
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools();
 }
 
 // Electron会在初始化完成并且准备好创建浏览器窗口时调用这个方法
---
+++ renderer/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: xuwei
+ * @Date: 2020-09-29 12:29:23
+ * @LastEditTime: 2020-09-29 12:44:06
+ * @LastEditors: xuwei
+ * @Description:
+ */
+
+// const { Notification } = require("electron");
+
+const sendBtn = document.getElementById("send_notify");
+sendBtn.addEventListener("click", () => {
+  // console.info("点击");
+});
